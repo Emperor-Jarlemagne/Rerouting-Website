717eee6872e8d3336804da9d35bbca8541b0114f
Emperor-Jarlemagne <104075576+Emperor-Jarlemagne@users.noreply.github.com>
2023-01-28 15:44:48 -0800

    Updates - Jan 28/2023

--- singlepage/singlepage/settings.py
+++ singlepage/singlepage/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!*p_y7r6o40p0a4#8$836$pxfb%gl7ph)9v%&dn$6*rktj-@2h'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["https://rerouting.tech"]
 
 # Application definition
 
@@ -39,7 +40,20 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'singlepage',
     'crispy_forms',
+    'sslserver',
 ]
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 518400
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_HSTS_PRELOAD = True
 
 CRISPY_TEMPLATE_PACK = 'uni_form'
 
