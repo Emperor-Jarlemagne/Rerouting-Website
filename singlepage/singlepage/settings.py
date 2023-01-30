a1d9ac645705296f711db42b8959d05ee0b9d557
Emperor-Jarlemagne <104075576+Emperor-Jarlemagne@users.noreply.github.com>
2023-01-29 16:34:59 -0800

    Updates - Jan 29/2023

--- singlepage/singlepage/settings.py
+++ singlepage/singlepage/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ["https://rerouting.tech", "github.com"]
+ALLOWED_HOSTS = [
+    "https://rerouting.tech",
+    "github.com",
+    "Emperor-Jarlemegne.github.io",
+    
+    ]
 
 # Application definition
 
